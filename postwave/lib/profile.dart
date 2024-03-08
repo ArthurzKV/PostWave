25ec54445dbe1b9bba750593bf132e4434f5413f
Arturo Ríos Ponce <rios.arturo0906@gmail.com>
2024-03-07 20:19:10 -0600

    Profile 0.1

--- postwave/lib/profile.dart
+++ postwave/lib/profile.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:image_picker/image_picker.dart';
 
 class NewTabPage extends StatefulWidget {
   const NewTabPage({Key? key}) : super(key: key);
@@ -35,8 +37,7 @@ class _NewTabPageState extends State<NewTabPage> {
         title: Text(userName),
         leading: IconButton(
           icon: const Icon(Icons.arrow_back),
-          onPressed: () => Navigator.pop(
-              context), // Manejar la pulsación del botón de retroceso
+          onPressed: () => Navigator.pop(context),
         ),
       ),
       body: Column(
@@ -45,7 +46,7 @@ class _NewTabPageState extends State<NewTabPage> {
             padding: const EdgeInsets.all(16.0),
             child: CircleAvatar(
               backgroundImage: NetworkImage(profilePicUrl),
-              radius: 50.0, // Ajustar el radio según sea necesario
+              radius: 50.0,
             ),
           ),
           Row(
@@ -57,7 +58,21 @@ class _NewTabPageState extends State<NewTabPage> {
             ],
           ),
           // Implementar la sección de publicaciones aquí
-          // ... (código para mostrar publicaciones)
+          Expanded(
+            child: GridView.count(
+              crossAxisCount: 3, // Mostrar 3 imágenes por fila
+              mainAxisSpacing: 4.0,
+              crossAxisSpacing: 4.0,
+              children: <Widget>[
+                Image.network(
+                    'https://www.innovasport.com/medias/IS-CV7889-409-1.png?context=bWFzdGVyfGltYWdlc3wxNDE0NzZ8aW1hZ2UvcG5nfGltYWdlcy9oM2UvaDliLzEwMTEwMzI1OTgxMjE0LnBuZ3wzY2YyZWE2MTIzMDg1MjJjMDRlNzYyYzI1MjE2YjQ3ZWJlNzg4N2EyYjJiOTM1Y2RhMDdmYjMxMDdjNzRkOWNh'), // Reemplazar con URLs reales
+                Image.network(
+                    'https://images.media-arocam.com/BEQdZdY_sZ4U_ogSdnpohfMRRik=/fit-in/1000x1000/W30122/WNY/I5IvmSp2_WNY.png'),
+                Image.network('https://url-imagen-3.com'),
+                // ... más imágenes según necesitesda
+              ],
+            ),
+          ),
         ],
       ),
     );
